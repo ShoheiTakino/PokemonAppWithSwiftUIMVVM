9f2985a52444f54ab846b2987355a4f2dd014bc9
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-05 21:46:28 +0900

    Initial Commit

---
+++ PokemonAppWithSwiftUIMVVM/PokemonAppWithSwiftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppWithSwiftUIMVVMApp.swift
+//  PokemonAppWithSwiftUIMVVM
+//
+//  Created by 滝野翔平 on 2023/04/05.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppWithSwiftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

01b36f4ed9dbede162275751f43b60c87e7229b9
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-07 00:31:27 +0900

    リスト

---
+++ PokemonAppWithSwiftUIMVVM/Presentation/PokemonList/PokemonListView.swift
@@ -0,0 +1,33 @@
+//
+//  PokemonListView.swift
+//  PokemonAppWithSwiftUIMVVM
+//
+//  Created by 滝野翔平 on 2023/04/05.
+//
+
+import SwiftUI
+
+struct PokemonListView: View {
+
+        @StateObject private var viewModel = PokemonDataViewModel()
+
+        var body: some View {
+            NavigationView {
+                List(viewModel.posts) { post in
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(post.title)
+                            .font(.headline)
+                        Text(post.body)
+                            .font(.subheadline)
+                    }
+                }
+                .navigationBarTitle("List")
+            }
+        }
+}
+
+struct PokemonListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonListView()
+    }
+}

80bcaaafa776e2c14eb3b15da3bb24fa88d88c69
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-07 00:30:44 +0900

    詳細画面

---
+++ PokemonAppWithSwiftUIMVVM/Presentation/PokemonDetail/PokemonDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonDetailView.swift
+//  PokemonAppWithSwiftUIMVVM
+//
+//  Created by 滝野翔平 on 2023/04/05.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonDetailView()
+    }
+}

f9741166aa45f6574c41d3a8d45c50f328e88740
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-07 00:29:58 +0900

    コンスタンツ

---
+++ PokemonAppWithSwiftUIMVVM/Core/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  PokemonAppWithSwiftUIMVVM
+//
+//  Created by 滝野翔平 on 2023/04/06.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let pokeAPIUrl = "https://pokeapi.co/api/v2/pokemon//"
+    static let jsonUrl = "https://jsonplaceholder.typicode.com"
+}

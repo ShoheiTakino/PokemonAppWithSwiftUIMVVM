fd3c3c97b60c5b5711ac285d877bf3833599bda5
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-07 00:31:01 +0900

    グリッド画面

---
+++ PokemonAppWithSwiftUIMVVM/Presentation/PokemonLazyVGrid/PokemonLazyVGridView.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonLazyVGridView.swift
+//  PokemonAppWithSwiftUIMVVM
+//
+//  Created by 滝野翔平 on 2023/04/05.
+//
+
+import SwiftUI
+
+struct PokemonLazyVGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonLazyVGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonLazyVGridView()
+    }
+}

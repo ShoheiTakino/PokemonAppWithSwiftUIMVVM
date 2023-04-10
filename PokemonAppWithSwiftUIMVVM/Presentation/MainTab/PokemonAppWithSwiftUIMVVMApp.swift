865450c95154de04c06e84dc4de1fc756f7ca3c0
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-10 22:30:56 +0900

    アイコンと画面追加

--- PokemonAppWithSwiftUIMVVM/Presentation/MainTab/PokemonAppWithSwiftUIMVVMApp.swift
+++ PokemonAppWithSwiftUIMVVM/Presentation/MainTab/PokemonAppWithSwiftUIMVVMApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PokemonAppWithSwiftUIMVVMApp: App {
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            LaunchScreen()
         }
     }
 }

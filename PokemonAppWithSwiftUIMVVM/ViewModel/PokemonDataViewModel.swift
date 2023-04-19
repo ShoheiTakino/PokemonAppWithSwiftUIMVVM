4b1f1d7949697d5d183cb54521404c9ae0064d99
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-20 00:12:21 +0900

    viewmodel

--- PokemonAppWithSwiftUIMVVM/ViewModel/PokemonDataViewModel.swift
+++ PokemonAppWithSwiftUIMVVM/ViewModel/PokemonDataViewModel.swift
@@ -9,33 +9,40 @@ import SwiftUI
 import Combine
 
 final class PokemonDataViewModel: ObservableObject {
-    @Published var posts = [Post]()
     @Published var pokemonList: [Pokemon] = []
-
-    private var cancellables = Set<AnyCancellable>()
-
-    init() {
-        fetchPosts()
-    }
-
-    func fetchPosts() {
-        FetchPokemonData.fetchPosts()
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print("Error: \(error.localizedDescription)")
-                }
-            } receiveValue: { [weak self] posts in
-                self?.posts = posts
+    let apiClient = PokemonApiClient()
+    
+    func fetchPokemonData() async -> [Pokemon]? {
+        DispatchQueue.main.async {
+            self.pokemonList = []
+        }
+        var urlList: [URL] = []
+        urlList = createPokemonUrlList()
+        do {
+            for i in 0..<urlList.count {
+                let (data, _) = try await URLSession.shared.data(from: urlList[i])
+                let decoder = JSONDecoder()
+                let pokemon = try decoder.decode(Pokemon.self, from: data)
+                // 以下の処理で、警告は修正できるが時々pokemonの画像を処理できない。
+//                DispatchQueue.main.async {
+//                    self.pokemonList.append(pokemon)
+//                }
+                pokemonList.append(pokemon)
             }
-            .store(in: &cancellables)
+            return pokemonList
+        } catch {
+            
+        }
+        return nil
     }
     
-    func fetchPokemonList() async {
-        let pokemonList = await FetchPokemonData.fetchPokemonRequest()
-        self.pokemonList = pokemonList
+    /// urlを150体分生成する処理
+    private func createPokemonUrlList() -> [URL] {
+        var urlList: [URL] = []
+        let maxPokemon = 150
+        for i in 1...maxPokemon {
+            urlList.append(URL(string: "https://pokeapi.co/api/v2/pokemon/\(i)/")!)
+        }
+        return urlList
     }
 }

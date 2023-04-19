3fc75081b8f9eede222d36089219cde9a1f150cc
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-20 00:11:15 +0900

    クライアント

---
+++ PokemonAppWithSwiftUIMVVM/APIClient/PokemonApiClient.swift
@@ -0,0 +1,40 @@
+//
+//  PokemonApiClient.swift
+//  PokemonAppWithSwiftUIMVVM
+//
+//  Created by 滝野翔平 on 2023/04/19.
+//
+
+import Foundation
+
+final class PokemonApiClient: ObservableObject {
+    @Published var pokemonList: [Pokemon] = []
+    
+    func fetchPokemonData() async -> [Pokemon]? {
+        pokemonList = []
+        var urlList: [URL] = []
+        urlList = createPokemonUrlList()
+        do {
+            for i in 0..<urlList.count {
+                let (data, _) = try await URLSession.shared.data(from: urlList[i])
+                let decoder = JSONDecoder()
+                let pokemon = try decoder.decode(Pokemon.self, from: data)
+                pokemonList.append(pokemon)
+            }
+            return pokemonList
+        } catch {
+            
+        }
+        return nil
+    }
+    
+    /// urlを150体分生成する処理
+    private func createPokemonUrlList() -> [URL] {
+        var urlList: [URL] = []
+        let maxPokemon = 150
+        for i in 1...maxPokemon {
+            urlList.append(URL(string: "https://pokeapi.co/api/v2/pokemon/\(i)/")!)
+        }
+        return urlList
+    }
+}

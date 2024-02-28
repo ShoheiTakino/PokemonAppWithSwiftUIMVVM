9b376dec849175b606b7d069ecf8b976209325bd
ShoheiTakino <shohei0522penguin@gmail.com>
2024-02-28 20:07:54 +0900

    ✨エラー追加

---
+++ PokemonAppWithSwiftUIMVVM/Model/PokemonFetchError.swift
@@ -0,0 +1,12 @@
+//
+//  PokemonFetchError.swift
+//  PokemonAppWithSwiftUIMVVM
+//
+//  Created by 滝野翔平 on 2024/02/27.
+//
+
+import Foundation
+
+enum PokemonFetchError: Error {
+    case unknown
+}

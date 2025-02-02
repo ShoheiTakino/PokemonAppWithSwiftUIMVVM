19cdaf7c93d0a024bc51b7c2a47260a682cc77c0
ShoheiTakino <shohei0522penguin@gmail.com>
2025-02-02 22:42:02 +0900

    update list

--- PokemonAppWithSwiftUIMVVM/Presentation/FavoritePokemonList/FavoritePokemonListView.swift
+++ PokemonAppWithSwiftUIMVVM/Presentation/FavoritePokemonList/FavoritePokemonListView.swift
@@ -2,9 +2,41 @@ import SwiftUI
 
 struct FavoritePokemonListView: View {
     
-    @StateObject var viewModel: FavoritePokemonListViewModel
+    var viewModel: FavoritePokemonListViewModel
+
+    private enum Const {
+        static let columns: [GridItem] = Array(
+            repeating: .init(
+                .flexible(),
+                spacing: 10,
+                alignment: .center
+            ),
+            count: 1
+        )
+    }
 
     var body: some View {
-        Text("Hello, World!")
+        ScrollView(.vertical) {
+            LazyVGrid(columns: Const.columns) {
+                ForEach(viewModel.pokemons) { pokemon in
+                    HStack {
+                        NavigationLink(destination: PokemonDetailView(viewModel: .init(pokemonStore: viewModel.pokemonStore, pokemonDispatcher: viewModel.pokemonDispatcher, pokemon: pokemon))) {
+                            CacheAsyncImage(url: pokemon.imageUrl)
+                                .frame(width: 100, height: 100)
+                        }
+                        Text(pokemon.name)
+                    }
+                    .overlay(alignment: .topTrailing) {
+                        FavoriteButtonView(
+                            isFavorite: pokemon.isFavorite,
+                            onTapFavoriteButton: {
+                                viewModel.onTapFavorite(pokemon)
+                            }
+                        )
+                    }
+                }
+            }
+        }
+        .toolbar(.hidden, for: .tabBar)
     }
 }
--- PokemonAppWithSwiftUIMVVM/Presentation/FavoritePokemonList/FavoritePokemonListViewModel.swift
+++ PokemonAppWithSwiftUIMVVM/Presentation/FavoritePokemonList/FavoritePokemonListViewModel.swift
@@ -1,19 +1,35 @@
 import Foundation
+import Combine
+import Observation
 
-final class FavoritePokemonListViewModel: ObservableObject {
+@Observable
+final class FavoritePokemonListViewModel {
 
-    @Published
-    var pokemonStore: PokemonStore
-    let pokemonFavoriteStore: PokemonFavoriteStore
+    let pokemonStore: PokemonStore
     let pokemonDispatcher: PokemonDispatcher
+
+    var pokemons: [PokemonEntity] = []
+    private(set) var cancellables: Set<AnyCancellable> = Set()
 
     init(
         pokemonStore: PokemonStore,
-        pokemonFavoriteStore: PokemonFavoriteStore,
         pokemonDispatcher: PokemonDispatcher
     ) {
         self.pokemonStore = pokemonStore
-        self.pokemonFavoriteStore = pokemonFavoriteStore
         self.pokemonDispatcher = pokemonDispatcher
+
+        self.pokemonStore.$pokemonList
+            .receive(on: DispatchQueue.main)
+            .removeDuplicates()
+            .map { $0.filter { $0.isFavorite } }
+            .sink { [weak self] updatedList in
+                guard let self = self else { return }
+                self.pokemons = updatedList
+            }
+            .store(in: &cancellables)
+    }
+
+    func onTapFavorite(_ pokemon: PokemonEntity) {
+        pokemonDispatcher.updateFavorite(pokemon)
     }
 }

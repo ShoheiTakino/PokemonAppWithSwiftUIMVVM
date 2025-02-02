2800a87c2c01cdd95af23af3e9e2c78e67805546
ShoheiTakino <shohei0522penguin@gmail.com>
2025-02-02 22:39:39 +0900

    improve fetcher

--- PokemonAppWithSwiftUIMVVM/APIClient/PokemonFetcher.swift
+++ PokemonAppWithSwiftUIMVVM/APIClient/PokemonFetcher.swift
@@ -1,13 +1,18 @@
 import Foundation
 
 struct PokemonFetcher {
-    func fetchPokemonDataFrom(_ pokemonId: Int) async throws -> Pokemon {
-        let url: URL = {
-            URL(string: "\(Const.pokeAPIUrl)\(pokemonId)/")!
-        }()
+    func fetchPokemonDataFrom(_ pokemonId: Int) async throws -> PokemonResponse {
+
+        guard let url = URL(string: "\(Const.pokeAPIUrl)\(pokemonId)/") else {
+            throw URLGenerateError.invalidURL
+        }
 
         let (data, _) = try await URLSession.shared.data(from: url)
-        let pokemon = try JSONDecoder().decode(Pokemon.self, from: data)
+        let pokemon = try JSONDecoder().decode(PokemonResponse.self, from: data)
         return pokemon
+    }
+
+    private enum URLGenerateError: Error {
+        case invalidURL
     }
 }

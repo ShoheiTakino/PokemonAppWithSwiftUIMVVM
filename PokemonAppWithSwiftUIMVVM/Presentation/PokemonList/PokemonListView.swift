364e529c4cf8cda97556954a31fcee5d2fcf398b
ShoheiTakino <shohei0522penguin@gmail.com>
2025-02-02 22:41:45 +0900

    update list view to three calums grid

--- PokemonAppWithSwiftUIMVVM/Presentation/PokemonList/PokemonListView.swift
+++ PokemonAppWithSwiftUIMVVM/Presentation/PokemonList/PokemonListView.swift
@@ -1,43 +1,51 @@
-//
-//  PokemonListView.swift
-//  PokemonAppWithSwiftUIMVVM
-//
-//  Created by 滝野翔平 on 2023/04/05.
-//
-
 import SwiftUI
 
 struct PokemonListView: View {
-    @StateObject var viewModel: PokemonListViewModel
+
+    var viewModel: PokemonListViewModel
+
+    private enum Const {
+        static let columns: [GridItem] = Array(
+            repeating: .init(
+                .flexible(),
+                spacing: 10,
+                alignment: .center
+            ),
+            count: 3
+        )
+    }
 
     var body: some View {
         NavigationStack {
-            List(viewModel.pokemonStore.pokemonList) { pokemon in
-                HStack {
-                    AsyncImage(url: URL(string: pokemon.sprites.frontImage)) { image in
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 40)
-                    } placeholder: {
-                        ProgressView()
+            ScrollView(.vertical) {
+                LazyVGrid(columns: Const.columns) {
+                    ForEach(viewModel.pokemons) { pokemon in
+                        NavigationLink(destination: PokemonDetailView(viewModel: .init(pokemonStore: viewModel.pokemonStore, pokemonDispatcher: viewModel.pokemonDispatcher, pokemon: pokemon))) {
+                            VStack(alignment: .center, spacing: 4) {
+                                CacheAsyncImage(url: pokemon.imageUrl)
+                                    .frame(height: 80)
+                                Text(pokemon.name)
+                                    .foregroundStyle(.black)
+                            }
+                        }
+                        .overlay(alignment: .topTrailing) {
+                            FavoriteButtonView(
+                                isFavorite: pokemon.isFavorite,
+                                onTapFavoriteButton: {
+                                    viewModel.onTapFavorite(pokemon)
+                                }
+                            )
+                        }
                     }
-                    Text(pokemon.name)
-                    NavigationLink(destination: PokemonDetailView(pokemon: pokemon)) {
-                        
-                    }
-                }
-                .onAppear {
-                    print("ああああ", pokemon.id)
                 }
             }
-            .navigationBarTitle("List")
+            .navigationBarTitle("Grid three columns")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) { // 右側に配置
-                    NavigationLink(destination: FavoritePokemonListView(viewModel: .init(pokemonStore: viewModel.pokemonStore, pokemonFavoriteStore: viewModel.pokemonFavoriteStore, pokemonDispatcher: viewModel.pokemonDispatcher))) {
-                        Image(systemName: "plus") // ボタンのアイコン（システムアイコンを使用）
-                            .foregroundColor(.blue)
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: FavoritePokemonListView(viewModel: .init(pokemonStore: viewModel.pokemonStore, pokemonDispatcher: viewModel.pokemonDispatcher))) {
+                        Image(systemName: "rectangle.grid.1x2")
+                            .foregroundColor(.primary)
                     }
                 }
             }
@@ -45,8 +53,48 @@ struct PokemonListView: View {
     }
 }
 
-//struct PokemonListView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PokemonListView()
-//    }
-//}
+struct CacheAsyncImage: View {
+    let url: URL
+
+    @State private var image: Image?
+
+    var body: some View {
+        if let image = image {
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        } else {
+            AsyncImage(url: url) { phase in
+                switch phase {
+                case .success(let loadedImage):
+                    loadedImage
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                case .failure:
+                    ProgressView()
+                case .empty:
+                    ProgressView()
+                @unknown default:
+                    ProgressView()
+                }
+            }
+            .onAppear {
+                if let cachedImage = ImageCacher.shared[url] {
+                    image = cachedImage
+                }
+            }
+        }
+    }
+}
+
+final class ImageCacher {
+    static let shared = ImageCacher()
+    private init() {}
+
+    private var cache: [URL: Image] = [:]
+
+    subscript(url: URL) -> Image? {
+        get { cache[url] }
+        set { cache[url] = newValue }
+    }
+}
--- PokemonAppWithSwiftUIMVVM/Presentation/PokemonList/PokemonListViewModel.swift
+++ PokemonAppWithSwiftUIMVVM/Presentation/PokemonList/PokemonListViewModel.swift
@@ -1,26 +1,35 @@
 import Foundation
+import Combine
+import Observation
 
-final class PokemonListViewModel: ObservableObject {
+@Observable
+final class PokemonListViewModel {
 
-    @Published
-    var pokemonStore: PokemonStore
-    let pokemonFavoriteStore: PokemonFavoriteStore
+    let pokemonStore: PokemonStore
     let pokemonDispatcher: PokemonDispatcher
+    var pokemons: [PokemonEntity] = []
+
+    private var cancellables: Set<AnyCancellable> = Set()
 
     init(
         pokemonStore: PokemonStore,
-        pokemonFavoriteStore: PokemonFavoriteStore,
         pokemonDispatcher: PokemonDispatcher
     ) {
         self.pokemonStore = pokemonStore
-        self.pokemonFavoriteStore = pokemonFavoriteStore
         self.pokemonDispatcher = pokemonDispatcher
-    }
-}
 
-extension PokemonListViewModel {
-    @MainActor
-    func onAppear() {
-        print("ああああ", pokemonStore.pokemonList)
+        // pokemonStore の変更を監視
+        self.pokemonStore.$pokemonList
+            .receive(on: DispatchQueue.main)
+            .removeDuplicates()
+            .sink { [weak self] updatedList in
+                guard let self = self else { return }
+                self.pokemons = updatedList
+            }
+            .store(in: &cancellables)
+    }
+
+    func onTapFavorite(_ pokemon: PokemonEntity) {
+        pokemonDispatcher.updateFavorite(pokemon)
     }
 }
